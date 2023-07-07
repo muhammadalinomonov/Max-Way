ce98628b1d5a840e195651775b2f69af0f40c7e6
Muhammadali <muhammadalinomonov837@gmail.com>
2023-07-07 10:59:29 +0500

    filtering is slow

--- app/src/main/java/uz/gita/my_max_way_uz/ui/theme/Theme.kt
+++ app/src/main/java/uz/gita/my_max_way_uz/ui/theme/Theme.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -63,7 +64,7 @@ fun MaxWayTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = R.color.statusbarcolor
+            window.statusBarColor = statusBarColor.toArgb()
             window.navigationBarColor = R.color.statusbarcolor
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
